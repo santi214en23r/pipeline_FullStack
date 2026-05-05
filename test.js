d10b6939d0991f4015ab1295db2e2dc8436775cf
santi214en23r <anjasantiagoma@ittepic.edu.mx>
2026-05-04 19:35:52 -0700

    Agregando scripts

---
+++ test.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const options = {
+  hostname: "localhost",
+  port: 3000,
+  path: "/api/products",
+  method: "GET"
+};
+
+const req = http.request(options, (res) => {
+  if (res.statusCode === 403 || res.statusCode === 401) {
+    console.log("Test OK, la API protegio la ruta correctamente");
+    process.exit(0);
+    } else {
+    console.log(`Test Failed`);
+    process.exit(1);
+  }
+});
+
+req.on("error", (error) => {
+  console.log(`Error en la solicitud: ${error.message}`);
+  process.exit(1);
+});
+
+req.end();
